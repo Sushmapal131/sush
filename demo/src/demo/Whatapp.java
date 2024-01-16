e9b6bb20625039017c09a48eebb1e45a1410a4b5
sdac-20 <sdac-20@sdac-20-PC>
2024-01-16 09:05:00 +0530

    W v2.0

--- demo/src/demo/Whatapp.java
+++ demo/src/demo/Whatapp.java
@@ -3,6 +3,7 @@ package demo;
 public class Whatapp {
 public static void main(String[] args) {
 	System.out.println("Whatsapp intigration 1.0");
+	System.out.println("Whatsapp intigration 2.0");
 	
 }
 }
